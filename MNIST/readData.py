1e535dc19eb076960e21f679e5d629cdfdefa452
consider1001 <consider1001@163.com>
2015-03-11 10:00:59 +0800

    add navie KNN model

--- MNIST/readData.py
+++ MNIST/readData.py
@@ -1,16 +1,28 @@
+'''
+read data
+'''
 __author__ = 'Li'
+
+
 
 from numpy import *
 import csv
+import viewTestData
 
-lable = []
-
-def read(filename):
-    fcsv = open('train.csv','rb')
+def readTrainData(filename, numberOfLines):
+    '''
+    read data from .csv file
+    :param filename: the name of the .csv file
+    :param numberOfLines:  how many lines to read from the .csv file
+    :return: labelArray :  the label data in 1*n matrix
+    :return: arr :    the data in 1*n matrix
+    '''
+    label = []
+    fcsv = open(filename,'rb')
     lineNumber = 0
     for row in csv.reader(fcsv):
         if lineNumber-lineNumber/1000*1000 == 1:
-            print 'linenumber'
+            print 'readTrainData.py linenumber'
             print lineNumber
         if lineNumber == 0:
             lineNumber = lineNumber + 1
@@ -19,20 +31,54 @@ def read(filename):
         #create train data array
         if lineNumber == 1:
             arr = array(row[1:])
-            lable.append(row[0])
+            label.append(row[0])
+            if lineNumber == numberOfLines:
+                break
             lineNumber = lineNumber + 1
             continue
         arr = vstack((arr, array(row[1:])))
-        #create train lable array
-        lable.append(row[0])
+        #create train label array
+        label.append(row[0])
         # if lineNumber == 2:
         #     print 'break'
         #     break
         lineNumber = lineNumber + 1
-    return lable, arr
+        if lineNumber == numberOfLines + 1:  #consider the first name line
+            break
+    labelArray = array(label)
+    return labelArray, arr
+
+def readTestData(filename, numberOfLines):
+    '''
+    read test Data
+    :param filename:
+    :param numbersOfLines:
+    :return:
+    '''
+    fcsv = open(filename,'rb')
+    lineNumber = 0
+    for row in csv.reader(fcsv):
+        if lineNumber-lineNumber/1000*1000 == 1:
+            print 'readTestData.py linenumber'
+            print lineNumber
+        if lineNumber == 0:
+            lineNumber = lineNumber + 1
+            continue
+        row = map(int, row)
+        #create train data array
+        if lineNumber == 1:
+            arr = array(row[:])
+            if lineNumber == numberOfLines:
+                print 'break'
+                break
+            lineNumber = lineNumber + 1
+            continue
+        arr = vstack((arr, array(row[:])))
+        lineNumber = lineNumber + 1
+        if lineNumber == numberOfLines + 1:  #consider the first name line
+            break
+    return arr
+
 if __name__ == '__main__':
-    lable, arr = read('train.csv')
-    print 'lable'
-    print lable
-    print 'array'
-    print arr.shape
+    a = readTestData('test.csv',10)
+    print a
