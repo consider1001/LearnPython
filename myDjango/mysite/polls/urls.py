69d34a81ccf2d6efe03cbedb1975518be3a5c1b3
consider1001 <consider1001@163.com>
2015-04-03 11:59:32 +0800

    tourial over

--- myDjango/mysite/polls/urls.py
+++ myDjango/mysite/polls/urls.py
@@ -2,15 +2,11 @@ __author__ = 'liguo'
 
 from django.conf.urls import patterns, url
 
-from polls import views
+from . import views
 
-urlpatterns = patterns('',
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-)
+]
